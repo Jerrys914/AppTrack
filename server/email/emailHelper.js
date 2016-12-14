73e9ad47d62d9e77f9944e3fb9ce0e36dd9f68a0
Jerry Shanahan <jerry.shanahan@gmail.com>
2016-12-13 19:29:23 -0500

    fix(changed email date check):

--- server/email/emailHelper.js
+++ server/email/emailHelper.js
@@ -15,15 +15,15 @@ var weeklyReminderSender = () => { // fn called in server.js when server starts
       Users.find({username:reg}, (err, data) => { // find user where username equals regEx.. will return all users
         let users = data; // set users to returned array
         for(let i = 0; i < users.length; i++) { // for each user in the DB
-          Apps.find({userId:users[index].id}, (err, apps) => { // find all apps for that user
+          Apps.find({userId:users[i].id}, (err, apps) => { // find all apps for that user
             let userApps = apps; // set userApps to array of returned applications
             if(userApps.length > 0) { // if the user has open applications
-              emails.send(users[index], userApps); // call email.send function that is in emailModel.js passing in the user and array of their apps
+              emails.send(users[i], userApps); // call email.send function that is in emailModel.js passing in the user and array of their apps
             }
           });
         }
       });
-    } else if(!(day === 2 && hour === 11)){ // if the day in not tuesday and the hour is not 3pm
+    } else if(!(day === 2 && hour === 15)){ // if the day in not tuesday and the hour is not 3pm
       flag = false;
     }
   }, 1000 * 60 * 20); // run setInterval every 20 minutes
